8a98a8ca40900a7118fe3e4037b8bd2ccfcbe7d0
Felix Mairamhof <feberg.k@gmail.com>
2024-08-12 08:06:42 +0200

    Serve Backend in Deploy

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-import adminRoutes from './routes.js'; // Adjust path as necessary
+import path from 'path';
+import adminRoutes from './routes.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Configure CORS
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173', // Change to your deployed domain if needed
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type,Authorization'
 }));
 
-app.use(express.json()); // Middleware to parse JSON request bodies
-app.use('/api', adminRoutes); // Prefix all routes with '/api'
+app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// API routes
+app.use('/api', adminRoutes);
+
+// Serve the React app for other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
